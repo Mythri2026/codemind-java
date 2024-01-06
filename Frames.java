2a8fbf713c5caec8ccca4cb95122e211837894a8
MYTHRI <119866826+Mythri2026@users.noreply.github.com>
2024-01-06 18:32:14 +0530

    import java.util.Scanner; public class Code {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         int x=sc.nextInt();         int c=(2*(a+b))*x;         System.out.println(c);     } }

---
+++ Frames.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Code
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int x=sc.nextInt();
+        int c=(2*(a+b))*x;
+        System.out.println(c);
+    }
+}
