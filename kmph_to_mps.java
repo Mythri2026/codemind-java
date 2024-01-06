d665359cecffe310698d11e67316c76cda530e2d
MYTHRI <119866826+Mythri2026@users.noreply.github.com>
2024-01-06 18:54:18 +0530

    import java.util.Scanner; public class Code {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         double b=(0.2777777777777778)*a;         System.out.printf("%.2f",b);     } }

---
+++ kmph_to_mps.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Code
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        double b=(0.2777777777777778)*a;
+        System.out.printf("%.2f",b);
+    }
+}
