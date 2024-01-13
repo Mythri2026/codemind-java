9ca84d222ba6c4710b2f0dce5149dbdaf4d71838
MYTHRI <119866826+Mythri2026@users.noreply.github.com>
2024-01-13 14:39:07 +0530

    import java.util.Scanner; public class Code {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int m,n;         n=sc.nextInt();         m=sc.nextInt();         for(int i=n;i<=m;i++)         {             System.out.print(i + " ");         }     } }

--- A_to_B.java
+++ A_to_B.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 public class Code
- {
-     public static void main(String args[])
-     {
-         Scanner sc=new Scanner(System.in);
-         int n,m;
-         m=sc.nextInt();
-         n=sc.nextInt();
-         for(int i=m;i<=n;i++)
-         {
-             System.out.print(i + " ");
-         }
-     }
- }
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int m,n;
+        n=sc.nextInt();
+        m=sc.nextInt();
+        for(int i=n;i<=m;i++)
+        {
+            System.out.print(i + " ");
+        }
+    }
+}
