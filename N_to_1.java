8d27f125aeda7759741d845478fef26e45f042ed
MYTHRI <119866826+Mythri2026@users.noreply.github.com>
2024-01-13 14:32:16 +0530

    import java.util.Scanner; public class Code  {      public static void main(String args[])      {          Scanner sc=new Scanner(System.in);          int n;          n=sc.nextInt();          for(int i=n;i>0;--i)          {              System.out.print(i + " ");          }      }  }

---
+++ N_to_1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Code
+ {
+     public static void main(String args[])
+     {
+         Scanner sc=new Scanner(System.in);
+         int n;
+         n=sc.nextInt();
+         for(int i=n;i>0;--i)
+         {
+             System.out.print(i + " ");
+         }
+     }
+ }
