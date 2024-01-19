ac10dcc5d138a05bf88cd1966c4c48c14ab49399
MYTHRI <119866826+Mythri2026@users.noreply.github.com>
2024-01-19 20:57:35 +0530

    import java.util.Scanner; public class Code {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,b;         a=sc.nextInt();         b=sc.nextInt();         double loss=(a-b)/(a*1.0);         System.out.printf("%.2f",loss*100);     } }

--- Loss_Percentage.java
+++ Loss_Percentage.java
@@ -4,8 +4,9 @@ public class Code
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
-        int b=sc.nextInt();
+        int a,b;
+        a=sc.nextInt();
+        b=sc.nextInt();
         double loss=(a-b)/(a*1.0);
         System.out.printf("%.2f",loss*100);
     }
